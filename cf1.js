874fe231c3e7c8c268a549fd922fa3f1cf39f777
jandersen747 <jonandersenofficial@hotmail.com>
2020-09-01 12:29:02 +0200

    opgave

---
+++ cf1.js
@@ -0,0 +1,78 @@
+'use strict';
+
+let tails = 0;
+let heads = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+document.write('heads: ' + heads);
+document.write('tails: ' + tails);
